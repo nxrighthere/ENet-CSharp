d11e03d07209fb4a37836b9e23b558f3e4c37060
NX <nxrighthere@gmail.com>
2019-02-05 05:06:59 +0500

    Remove socket option duplicate

--- Source/Native/enet.h
+++ Source/Native/enet.h
@@ -3731,7 +3731,6 @@ extern "C" {
 		enet_socket_set_option(host->socket, ENET_SOCKOPT_BROADCAST, 1);
 		enet_socket_set_option(host->socket, ENET_SOCKOPT_RCVBUF, ENET_HOST_RECEIVE_BUFFER_SIZE);
 		enet_socket_set_option(host->socket, ENET_SOCKOPT_SNDBUF, ENET_HOST_SEND_BUFFER_SIZE);
-		enet_socket_set_option(host->socket, ENET_SOCKOPT_IPV6_V6ONLY, 0);
 
 		if (address != NULL && enet_socket_get_address(host->socket, &host->address) < 0)
 			host->address = *address;

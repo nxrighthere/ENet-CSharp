87cc581ba76d35944f1a978d49e64bb535346626
NX <nxrighthere@gmail.com>
2019-05-05 17:28:03 +0500

    Add compilation variable for debugging

--- Source/Native/jni/Android.mk
+++ Source/Native/jni/Android.mk
@@ -4,6 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libenet
 LOCAL_SRC_FILES := ..\enet.c
 
+ifdef ENET_DEBUG
+	LOCAL_CFLAGS += -DENET_DEBUG
+endif
+
 ifdef ENET_LZ4
 	LOCAL_CFLAGS += -DENET_LZ4
 	LOCAL_SRC_FILES += ..\lz4\lz4.c
